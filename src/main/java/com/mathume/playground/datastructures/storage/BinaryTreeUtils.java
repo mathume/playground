798b87c67f60b6efa86dba687fee6fa3078c1076
Sebastian Mitterle <sebastianmitterle@mathume.com>
2017-02-15 07:37:25 +0100

    solved binary search tree problem

--- src/main/java/com/mathume/playground/datastructures/storage/BinaryTreeUtils.java
+++ src/main/java/com/mathume/playground/datastructures/storage/BinaryTreeUtils.java
@@ -1,6 +1,5 @@
 package com.mathume.playground.datastructures.storage;
 
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,14 +11,14 @@ public class BinaryTreeUtils {
     public static boolean checkBinarySearchTree(BinaryTree<Integer> t) {
         List<Integer> visited = new ArrayList<Integer>();
         try {
-            checkBinarySearchTree(visited, t.getRoot());
+            checkBinarySearchTree(visited, t.getRoot(), null);
             return true;
         } catch (IllegalArgumentException e) {
             return false;
         }
     }
 
-    private static void checkBinarySearchTree(List<Integer> track, Node<Integer> current) throws IllegalArgumentException {
+    private static void checkBinarySearchTree(List<Integer> track, Node<Integer> current, Integer previous) throws IllegalArgumentException {
         if (current != null) {
             Integer value = current.getValue();
             Node<Integer> left = current.getLeft();
@@ -28,25 +27,44 @@ public class BinaryTreeUtils {
                     || (right != null && value > right.getValue())) {
                 throw new IllegalArgumentException();
             } else {
-                addOrdered(track, value);
-                checkBinarySearchTree(track, left);
-                checkBinarySearchTree(track, right);
+                addChecked(track, value, previous);
+                checkBinarySearchTree(track, left, value);
+                checkBinarySearchTree(track, right, value);
             }
         }
     }
 
-    private static void addOrdered(List<Integer> l, Integer value) throws IllegalArgumentException {
+    private static void addChecked(List<Integer> l, Integer value, Integer previous) throws IllegalArgumentException {
         if (l != null && l.size() > 0) {
             Integer first = l.get(0);
             Integer last = l.get(l.size() - 1);
             if (value == first || value == last) {
                 throw new IllegalArgumentException();
             }
-            if (value < first) {
-                l.add(0, value);
+            if (previous != null) {
+                if ((value < first && previous > first)
+                        || (value > last && previous < last)) {
+                    throw new IllegalArgumentException();
+                }
             }
         } else {
+            addOrdered(l, value);
+        }
+    }
+
+    private static void addOrdered(List<Integer> l, Integer value) {
+        if (l.size() == 0) {
             l.add(value);
+        } else {
+            for (int i = 0; i < l.size(); i++) {
+                if (value < l.get(i)) {
+                    l.add(i, value);
+                    break;
+                }
+                if (i == l.size() - 1) {
+                    l.add(value);
+                }
+            }
         }
     }
 }
